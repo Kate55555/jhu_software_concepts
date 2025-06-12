e63b0c6b53c5dae0b043a5b2db3a8697ccd45028
Kate Borisova <eboriso1@jh.edu>
2025-06-12 21:58:52 +1200

    Add a test to test an order cost

--- module_4/tests/test_order.py
+++ module_4/tests/test_order.py
@@ -22,10 +22,30 @@ class TestOrder:
         """Assert order should not have yet been paid"""
         assert order_bucket.paid is True
     
+
     ### Test order __str__()
     # Test order should return a string containing customer full order and cost
     
+
     ### Test order input_pizza()
+    @pytest.mark.parametrize("pizza_parm, expected_cost", [
+        ({
+            "crust": "Thin", "sauce": ["Marinara"], 
+            "cheese": "Mozzarella", "toppings": ["Pineapple"]
+        }, 5 + 2 + 1),
+        ({
+            "crust": "Thin", "sauce": ["Marinara", "Liv Sauce"], 
+            "cheese": "Mozzarella", "toppings": ["Pepperoni", "Mushrooms"]
+        }, 5 + 2 + 5 + 2 + 3),
+        ({
+            "crust": "Thick", "sauce": ["Pesto", "Liv Sauce"], 
+            "cheese": "Mozzarella", "toppings": ["Pepperoni", "Mushrooms"]
+        }, 6 + 3 + 5 + 2 + 3),
+        ({
+            "crust": "Gluten Free", "sauce": ["Marinara", "Pesto", "Liv Sauce"], 
+            "cheese": "Mozzarella", "toppings": ["Pineapple", "Pepperoni", "Mushrooms"]
+        }, 8 + 2 + 3 + 5 + 1 + 2 + 3),
+    ])
     def test_cost_by_input(
             self, 
             pizza_parm: dict, 
@@ -35,14 +55,15 @@ class TestOrder:
         """Test method should update cost"""
         order_bucket.input_pizza(
             pizza_parm["crust"],
-            pizza_parm["sause"],
+            pizza_parm["sauce"],
             pizza_parm["cheese"],
             pizza_parm["toppings"]
         )
         assert order_bucket.cost == expected_cost
     
+
     ### Test order order_paid()
     def test_is_paid_oder(self, order_bucket: Order):
         """Test method should update paid to true"""
-        order_bucket.set_paid()
+        order_bucket.order_paid()
         assert order_bucket.paid is True
