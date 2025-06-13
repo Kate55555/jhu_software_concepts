f30f2d3fe03de7a249d5cf0c0dcc0b18352507d4
Kate Borisova <eboriso1@jh.edu>
2025-06-13 22:35:11 +1200

    Prepare placeholder classes for Pizza and Order as per the reqs

---
+++ module_4/app/order.py
@@ -0,0 +1,22 @@
+class Order:
+    def __init__(self):
+        """Initialize a customer order"""
+        # Initialize order cost
+        pass
+
+
+    def __str__(self):
+        """Print a customer's complete order"""
+        pass
+
+
+    def input_pizza(self, crust, sause, cheese, toppings):
+        """Input the customer's order for a given pizza"""
+        # Initialize the pizza object and attach to the order
+        # Update the cost
+        pass
+
+
+    def order_paid(self):
+        """Set order as paid once payment has been closed"""
+        pass
---
+++ module_4/app/pizza.py
@@ -0,0 +1,20 @@
+class Pizza:
+    """Pizza objects and associated cost"""
+
+    def __init__(self, crust, sause, cheese, toppings):
+        """Initialize a pizza"""
+        # Set pizza variables
+        # Set cost to create
+        pass
+
+
+    def __str__(self):
+        """Print a pizza"""
+        # Print the cost of that pizza
+        pass
+
+
+    def cost(self):
+        """Determine the cost of a pizza"""
+        pass
+    
