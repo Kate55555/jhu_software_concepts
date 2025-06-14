734c63eeac3ef0caba89e8a5f76546a79ab7e6f8
Kate Borisova <eboriso1@jh.edu>
2025-06-15 00:15:42 +1200

    Add pizza code

--- module_4/app/pizza.py
+++ module_4/app/pizza.py
@@ -1,20 +1,87 @@
+"""This module contains a simple pizza class"""
+
 class Pizza:
     """Pizza objects and associated cost"""
 
-    def __init__(self, crust, sause, cheese, toppings):
-        """Initialize a pizza"""
+    # A nested dictionary of ingredient pricing
+    prices = {
+        "Crust": {
+            "Thin": 5,
+            "Thick": 6,
+            "Gluten Free": 8
+        },
+        "Sauce": {
+            "Marinara": 2, 
+            "Pesto": 3, 
+            "Liv Sauce": 5
+        },
+        "Cheese": {
+            "Mozzarella": 0
+        },
+        "Toppings": {
+            "Pineapple": 1, 
+            "Pepperoni": 2, 
+            "Mushrooms": 3
+        }
+    }
+
+
+    def __init__(
+            self, 
+            crust: str, 
+            sauce: list, 
+            cheese: str, 
+            toppings: list
+    ):
+        """Initialize a pizza
+        
+        :param crust: Pizza crust as one of the values: Thin, Thick, Gluten Free
+        :type crust: str
+        :pizza sauce: Pizza sause as at least one of the values: Marinara, Pesto, Liv Sauce
+        :type sauce: list
+        :param cheese: Pizza cheese, only Mozzarella supported
+        :type cheese: str
+        :param toppings: Pizza toppings as at least one of the values: Pineapple, Pepperoni, Mushrooms
+        :type toppings: list
+        :return: none
+        :rtype: none
+        """
         # Set pizza variables
+        self.crust = crust
+        self.sauce = sauce
+        self.cheese = cheese
+        self.toppings = toppings
+
         # Set cost to create
-        pass
+        self.selling_cost = self.prices["Crust"][self.crust] \
+            + sum(self.prices["Sauce"][sos] for sos in self.sauce) \
+            + self.prices["Cheese"][self.cheese] \
+            + sum(self.prices["Topping"][top] for top in self.toppings)
 
 
-    def __str__(self):
-        """Print a pizza"""
+    def __str__(self) -> str:
+        """Print a pizza
+        
+        :return: String escription of a pizza
+        :rtype: str
+        """
         # Print the cost of that pizza
-        pass
+        string = f"{self.crust}, "
+        for sos in self.sauce:
+            string += sos
+        string += f", {self.cheese} pizza with "
+        if len(self.toppings) == 1:
+            string += self.toppings[0]
+        else:
+            string += f"{', '.join(self.toppings[:-1])} and {self.toppings[-1]}"
+        string += f". Cost: ${self.cost}."
+        return string
 
 
-    def cost(self):
-        """Determine the cost of a pizza"""
-        pass
-    
+    def cost(self) -> int:
+        """Determine the cost of a pizza
+        
+        :return: Pizza cost
+        :rtype: int
+        """
+        return self.selling_cost
