0d0e30de4339736c6e581dd855b4f68648a08279
Kate Borisova <eboriso1@jh.edu>
2025-06-14 23:19:19 +1200

    Add order code

--- module_4/app/order.py
+++ module_4/app/order.py
@@ -1,22 +1,66 @@
+"""This module contains an order class"""
+from app.pizza import Pizza
+
 class Order:
     def __init__(self):
-        """Initialize a customer order"""
+        """Initialize a customer order
+
+        :return: none
+        :rtype: none
+        """
         # Initialize order cost
-        pass
+        self.cost = 0
+        # Initialize as an unpaid order
+        self.paid = False
+        # Initialize empty order
+        self.pizzas = []
 
 
-    def __str__(self):
-        """Print a customer's complete order"""
-        pass
+    def __str__(self) -> str:
+        """Print a customer's complete order
+
+        :return: Order details, including pizzas and costs
+        :rtype: str
+        """
+        string = "Customer Requested:\n"
+        for i in range(len(self.pizzas)):
+            string += f"{str(self.pizzas)}\n"
+        return string
 
 
-    def input_pizza(self, crust, sause, cheese, toppings):
-        """Input the customer's order for a given pizza"""
+    def input_pizza(
+            self, 
+            crust: str, 
+            sauce: list, 
+            cheese: str, 
+            toppings: list
+    ):
+        """Input the customer's order for a given pizza
+        
+        :param crust: Pizza crust as one of the values: Thin, Thick, Gluten Free
+        :type crust: str
+        :pizza sauce: Pizza sause as at least one of the values: Marinara, Pesto, Liv Sauce
+        :type sauce: list
+        :param cheese: Pizza cheese, only Mozzarella supported
+        :type cheese: str
+        :param toppings: Pizza toppings as at least one of the values: Pineapple, Pepperoni, Mushrooms
+        :type toppings: list
+        :return: none
+        :rtype: none
+        """
         # Initialize the pizza object and attach to the order
+        pizza = Pizza(crust, sauce, cheese, toppings)
+        self.pizzas.append(pizza)
+
         # Update the cost
-        pass
+        self.cost += pizza.cost()
 
 
     def order_paid(self):
-        """Set order as paid once payment has been closed"""
-        pass
+        """Set order as paid once payment has been closed
+
+        :return: none
+        :rtype: none
+        """
+        self.paid = True
+        
