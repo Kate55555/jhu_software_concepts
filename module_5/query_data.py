b3aa009e3cdda7c1eb12fb4c9288cd6b0545b25a
Kate Borisova <eboriso1@jh.edu>
2025-06-16 22:21:05 +1200

    Lint query_data.py

--- module_5/query_data.py
+++ module_5/query_data.py
@@ -1,9 +1,19 @@
+"""This module queries the database table Applicants"""
 from psycopg2 import OperationalError
 from create_table import create_connection
 
 
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
+def execute_read_query(dbconnection, query):
+    """Read data from the database as per the query
+    
+    :param dbconnection: Connection string
+    :type dbconnection: connection
+    :param query: Query expression
+    :type query: str
+    :return: int | float | None
+    :rtype: float
+    """
+    cursor = dbconnection.cursor()
     result = None
     try:
         cursor.execute(query)
@@ -11,12 +21,20 @@ def execute_read_query(connection, query):
         return result
     except OperationalError as e:
         print(f"The error '{e}' occurred")
+        return None
 
 
-def problem_1(connection):
+def problem_1(dbconnection):
+    """Answer the question #1 from the Assignemnt
+    
+    :param dbconnection: Connection string
+    :type dbconnection: connection
+    :return: Database entries for Spring 2025
+    :rtype: integer
+    """
     # 1. How many entries do you have in your database who have applied
     # for Spring 2025?
-    select_SP2025 = """
+    select_sp2025 = """
     SELECT
         COUNT(term)
     FROM
@@ -24,12 +42,19 @@ def problem_1(connection):
     WHERE
         term = 'Spring 2025'
     ;"""
-    apps_number = execute_read_query(connection, select_SP2025)
+    apps_number = execute_read_query(dbconnection, select_sp2025)
     for count in apps_number:
         return count[0]
 
 
-def problem_2(connection):
+def problem_2(dbconnection):
+    """Answer the question #2 from the Assignemnt
+    
+    :param dbconnection: Connection string
+    :type dbconnection: connection
+    :return: Percentage of international students
+    :rtype: float
+    """
     # 2. What percentage of entries are from international students
     # (not American or Other) (to two decimal places)?
     select_i18n = """
@@ -46,13 +71,20 @@ def problem_2(connection):
     FROM
         applicants
     ;"""
-    i18n_number = execute_read_query(connection, select_i18n)
+    i18n_number = execute_read_query(dbconnection, select_i18n)
     # Compute percentage
     i18n_share = i18n_number[0][0] / i18n_number[1][0] * 100
     return i18n_share
 
 
-def problem_3(connection):
+def problem_3(dbconnection):
+    """Answer the question #3 from the Assignemnt
+    
+    :param dbconnection: Connection string
+    :type dbconnection: connection
+    :return: Average GPA, GRE, GRE V, GRE AW
+    :rtype: float
+    """
     # 3. What is the average GPA, GRE, GRE V, GRE AW of applicants who
     # provide these metrics?
     select_averages = """
@@ -92,11 +124,18 @@ def problem_3(connection):
         gre_aw > 0
         AND gre_aw <= 6
     ;"""
-    averages = execute_read_query(connection, select_averages)
+    averages = execute_read_query(dbconnection, select_averages)
     return averages
 
 
-def problem_4(connection):
+def problem_4(dbconnection):
+    """Answer the question #4 from the Assignemnt
+    
+    :param dbconnection: Connection string
+    :type dbconnection: connection
+    :return: Average GPA of American students in Spring 2025
+    :rtype: float
+    """
     # 4. What is their average GPA of American students in Spring 2025?
     select_average = """
     SELECT
@@ -108,12 +147,19 @@ def problem_4(connection):
         AND gpa <= 4
         AND us_or_international = 'American'
     ;"""
-    average_gpa = execute_read_query(connection, select_average)
+    average_gpa = execute_read_query(dbconnection, select_average)
     for result in average_gpa:
         return result[0]
 
 
-def problem_5(connection):
+def problem_5(dbconnection):
+    """Answer the question #5 from the Assignemnt
+    
+    :param dbconnection: Connection string
+    :type dbconnection: connection
+    :return: Percentage of accepted applicants for Spring 2025
+    :rtype: float
+    """
     # 5. What percent of entries for Spring 2025 are Acceptances
     # (to two decimal places)?
     select_accepted = """
@@ -132,13 +178,20 @@ def problem_5(connection):
     WHERE
         term = 'Spring 2025'
     ;"""
-    accepted = execute_read_query(connection, select_accepted)
+    accepted = execute_read_query(dbconnection, select_accepted)
     # Compute percentage
     accepted_share = accepted[0][0] / accepted[1][0] * 100
     return accepted_share
 
 
-def problem_6(connection):
+def problem_6(dbconnection):
+    """Answer the question #6 from the Assignemnt
+    
+    :param dbconnection: Connection string
+    :type dbconnection: connection
+    :return: Average GPA of applicants in Spring 2025
+    :rtype: float
+    """
     # 6. What is the average GPA of applicants who applied for Spring 2025
     # who are Acceptances?
     select_average = """
@@ -152,15 +205,22 @@ def problem_6(connection):
         AND term = 'Spring 2025'
         AND status LIKE '%Accepted%'
     ;"""
-    average_gpa = execute_read_query(connection, select_average)
+    average_gpa = execute_read_query(dbconnection, select_average)
     for result in average_gpa:
         return result[0]
 
 
-def problem_7(connection):
+def problem_7(dbconnection):
+    """Answer the question #7 from the Assignemnt
+    
+    :param dbconnection: Connection string
+    :type dbconnection: connection
+    :return: Number of entries from applicants who applied to JHU CS
+    :rtype: int
+    """
     # 7. How many entries are from applicants who applied to JHU for a masters
     # degrees in Computer Science?
-    select_JHU = """
+    select_jhu = """
     SELECT
         COUNT(program)
     FROM
@@ -170,7 +230,7 @@ def problem_7(connection):
         AND program LIKE '%Computer Science%'
         AND degree LIKE '%Masters%'
     ;"""
-    jhu_masters_cs = execute_read_query(connection, select_JHU)
+    jhu_masters_cs = execute_read_query(dbconnection, select_jhu)
     for result in jhu_masters_cs:
         return result[0]
 
@@ -182,31 +242,31 @@ if __name__ == "__main__":
     )
 
     # Problem 1
-    print(f"1. Number of entries for Spring 2025: ")
+    print("1. Number of entries for Spring 2025: ")
     print(f"{problem_1(connection)}")
 
     # Problem 2
-    print(f"2. Entries from international students: ")
+    print("2. Entries from international students: ")
     print(f"{problem_2(connection):.2f}%")
 
     # Problem 3
-    print(f"3. Average GPA, GRE, GRE V, GRE AW: ")
+    print("3. Average GPA, GRE, GRE V, GRE AW: ")
     for score in problem_3(connection):
         print(f"{score[0]}: {score[1]:.2f}")
 
     # Problem 4
-    print(f"4. Average GPA of American students in Spring 2025: ")
+    print("4. Average GPA of American students in Spring 2025: ")
     print(f"{problem_4(connection):.2f}")
 
     # Problem 5
-    print(f"5. Percent of entries for Spring 2025 that are Acceptances: ")
+    print("5. Percent of entries for Spring 2025 that are Acceptances: ")
     print(f"{problem_5(connection):.2f}%")
 
     # Problem 6
-    print(f"6.  GPA of applicants in Spring 2025 that are accepted: ")
+    print("6.  GPA of applicants in Spring 2025 that are accepted: ")
     print(f"{problem_6(connection):.2f}")
 
     # Problem 7
-    print(f"7. Number of applicants who applied for Masters degrees " \
+    print("7. Number of applicants who applied for Masters degrees " \
           "in Computer Science at JHU: ")
     print(f"{problem_7(connection)}")
